07603e3b56023b3ff6d4c174201efacedaefe56c
potatoSalad21 <eeperpeepo@gmail.com>
2026-08-30 23:57:03 +0400

    feat(tokenizer): mapping file content to tokens

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use std::env;
+use std::fs;
+use std::process;
+
+enum OpType {
+    Right,
+    Left,
+    Inc,
+    Dec,
+    Output,
+    Input,
+    JmpIfZero,
+    JmpIfNonzero,
+}
+
+struct Op {
+    op_type: OpType,
+    operand: usize
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: {} <file.bf>", args[0]);
+        process::exit(1);
+    }
+
+    let file_path = &args[1];
+    let buf = fs::read_to_string(file_path).unwrap_or_else(|err| {
+        eprintln!("Error reading '{file_path}': {err}");
+        process::exit(1);
+    });
+
+    let ops: Vec<Op> = buf
+        .chars()
+        .filter_map(|ch| match ch {
+            '>' => Some(OpType::Right),
+            '<' => Some(OpType::Left),
+            '+' => Some(OpType::Inc),
+            '-' => Some(OpType::Dec),
+            '.' => Some(OpType::Output),
+            ',' => Some(OpType::Input),
+            '[' => Some(OpType::JmpIfZero),
+            ']' => Some(OpType::JmpIfNonzero),
+            _ => None
+        })
+        .map(|op_type| Op { op_type, operand: 1})
+        .collect();
 }
