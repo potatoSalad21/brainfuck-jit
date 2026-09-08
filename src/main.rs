97ad0659e2be1ca2b3d658bdd834685e126d0686
potatoSalad21 <eeperpeepo@gmail.com>
2026-09-08 23:59:52 +0400

    ref(lexing): lexer's next() + lexing logic refactored (better rust code)

--- src/main.rs
+++ src/main.rs
@@ -30,9 +30,9 @@ impl OpType {
     }
 
     fn is_repeatable(&self) -> bool {
-        matches!(
+        !matches!(
             self,
-            OpType::Right | OpType::Left | OpType::Inc | OpType::Dec)
+            OpType::JmpIfZero | OpType::JmpIfNonzero)
     }
 }
 
@@ -50,39 +50,13 @@ impl Lexer {
     fn new(buf: String) -> Self {
         Self { buf, pos: 0 }
     }
-}
 
-impl Iterator for Lexer {
-    type Item = Op;
-
-    fn next(&mut self) -> Option<Self::Item> {
+    fn next(&mut self) -> Option<char> {
         while self.pos < self.buf.len() {
             let ch = self.buf.as_bytes()[self.pos] as char;
             self.pos += 1;
-
-            if let Some(op_type) = OpType::from_char(ch) {
-                let mut count = 1;
-
-                if op_type.is_repeatable() {
-                    while self.pos < self.buf.len() {
-                        let next_ch = self.buf.as_bytes()[self.pos] as char;
-                        match OpType::from_char(next_ch) {
-                            Some(next_op) if next_op == op_type => {
-                                count += 1;
-                                self.pos += 1;
-                            }
-                            Some(_) => break,
-                            None => {
-                                self.pos += 1;
-                            }
-                        }
-                    }
-                }
-
-                return Some(Op {
-                    op_type,
-                    operand: count,
-                });
+            if OpType::from_char(ch).is_some() {
+                return Some(ch);
             }
         }
         None
@@ -102,13 +76,38 @@ fn main() {
         process::exit(1);
     });
 
-    let lexer = Lexer::new(buf);
-    let ops: Vec<Op> = lexer.collect();
+    let mut lexer = Lexer::new(buf);
+    let mut ops: Vec<Op> = Vec::new();
 
-    println!("parsed {} instructions", ops.len());
-    for op in ops {
-        println!("OpType: {:?}", op.op_type);
-        println!("operand: {}", op.operand);
-        println!();
+    let mut ch = lexer.next();
+    while let Some(curr) = ch {
+        if let Some(op_type) = OpType::from_char(curr) {
+            if op_type.is_repeatable() {
+                let mut count: usize = 1;
+                let mut next = lexer.next();
+
+                while next == Some(curr) {
+                    count += 1;
+                    next = lexer.next();
+                }
+
+                ops.push(Op {
+                    op_type,
+                    operand: count,
+                });
+                ch = next;
+            } else if op_type == OpType::JmpIfZero {
+                todo!("implement jmp forward");
+            } else if op_type == OpType::JmpIfNonzero {
+                todo!("implement jmp backwards");
+            }
+        } else {
+            ch = lexer.next();
+        }
+    }
+
+    for i in 0..ops.len() {
+        let op = ops.get(i).unwrap();
+        println!("{}: {:?} ({})", i, op.op_type, op.operand);
     }
 }
